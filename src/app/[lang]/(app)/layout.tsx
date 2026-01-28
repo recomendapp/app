3e092d171cccc1a868af387a4c1858a4f2f9d325
loup <loup.quehec@gmail.com>
2026-01-28 21:30:17 +0100

    fix wrong locale when skipping middleware and file doesnt exist

--- src/app/[lang]/(app)/layout.tsx
+++ src/app/[lang]/(app)/layout.tsx
@@ -1,10 +1,21 @@
 import { AppLayout } from "@/layouts/app-layout/AppLayout";
+import { routing } from "@/lib/i18n/routing";
+import { SupportedLocale } from "@/translations/locales";
+import { notFound } from "next/navigation";
 
 interface AppLayoutProps {
   children: React.ReactNode;
+  params: Promise<{ lang: SupportedLocale }>;
 };
 
-export default async function RootLayout({ children }: AppLayoutProps) {
+export default async function RootLayout({
+  children,
+  params,
+}: AppLayoutProps) {
+  const { lang } = await params;
+  if (routing.locales.includes(lang) === false) {
+    notFound();
+  }
   return (
     <AppLayout>
       {children}
